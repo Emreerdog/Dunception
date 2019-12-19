9fff4329d849509482b43b1d80a4e672a5bfa307
unknown <emreerdog@hotmail.com>
2019-12-19 23:45:23 +0300

    First push

---
+++ Source/Dunception/DunceptionGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DunceptionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUNCEPTION_API ADunceptionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
