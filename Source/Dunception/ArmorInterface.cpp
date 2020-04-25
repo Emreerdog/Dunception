77cad7e5d4bd27a7381810a021566711aa69219d
Mustafa Emre Erdog <emreerdog@hotmail.com>
2020-04-25 19:15:33 +0300

    Armor interface created but not modified yet

---
+++ Source/Dunception/ArmorInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ArmorInterface.h"
+
+// Add default functionality here for any IArmorInterface functions that are not pure virtual.
